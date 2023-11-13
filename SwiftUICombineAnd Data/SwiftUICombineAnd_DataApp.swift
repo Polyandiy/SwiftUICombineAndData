490a28a61f018ace174d62c71f120ba6d903f713
Polina Skuratova <skuratova250496@gmail.com>
2023-11-13 13:06:01 +0700

    Initial Commit

---
+++ SwiftUICombineAnd Data/SwiftUICombineAnd_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAnd_DataApp.swift
+//  SwiftUICombineAnd Data
+//
+//  Created by Поляндий on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAnd_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
